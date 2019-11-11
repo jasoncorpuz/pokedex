eca6e812cada5509775fe8a80f734eb612a0c158
Jason Corpuz <jason.corpuz@live.com>
2019-11-11 11:27:47 -0800

    ready for deployment

---
+++ server.js
@@ -0,0 +1,77 @@
+clrequire('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const POKEDEX = require('./pokedex.json')
+const cors = require('cors')
+const helmet = require('helmet')
+const app = express()
+
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+
+app.use(morgan(morganSetting))
+app.use(helmet())
+app.use(cors())
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+//middleware man
+app.use(function validateBearerToken(req,res,next) {
+    
+    const authToken = req.get('Authorization')
+    const apiToken = process.env.API_TOKEN
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request'})
+    }
+    next() //required or timeout
+})
+
+
+
+function handleGetTypes(req, res) {
+    res.json(validTypes)
+}
+
+app.get('/types', handleGetTypes)
+
+function handleGetPokemon(req,res) {
+    let response = POKEDEX.pokemon;
+
+    // filter our pokemon by name if name query param is present
+    if (req.query.name) {
+      response = response.filter(pokemon =>
+        // case insensitive searching
+        pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+      )
+    }
+  
+    // filter our pokemon by type if type query param is present
+    if (req.query.type) {
+      response = response.filter(pokemon =>
+        pokemon.type.includes(req.query.type)
+      )
+    }
+  
+    res.json(response)
+  }
+
+app.get('/pokemon', handleGetPokemon)
+
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+
+const PORT = process.env.PORT || 8000 // check if there is an enviromental variable, if not fall back to default
+
+app.listen(PORT, () => {
+    console.log(`Sever listening at ${PORT}`)
+})
+
+console.log(process.env.API_TOKEN)
